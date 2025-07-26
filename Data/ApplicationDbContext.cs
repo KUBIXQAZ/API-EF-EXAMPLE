6109f2e678cf93906922e3874c81d0894457efca
KUBIXQAZ <kubajozwik20027@gmail.com>
2025-07-26 14:08:47 +0200

    Created User and Product models with EF Core context

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using API_EF_JWT.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_EF_JWT.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<User> Users => Set<User>();
+        public DbSet<Product> Products => Set<Product>();
+    }
+}
